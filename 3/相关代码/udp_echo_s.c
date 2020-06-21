e85443504286315130f131cff8252c25b271790c
VanHWZ <van136682262@gmail.com>
2020-06-21 22:31:44 +0800

    commit 3

---
+++ 3/相关代码/udp_echo_s.c
@@ -0,0 +1,29 @@
+#include "unp.h"
+
+void dg_echo(int sockfd, struct sockaddr*pcliaddr, socklen_t clilen){
+    int n;
+    socklen_t len;
+    char mes[MAXLINE];
+    for(;;){
+        len=clilen;
+        n=Recvfrom(sockfd, mes, MAXLINE, 0, pcliaddr, &len);
+        mes[n]='\0';
+        printf("server receives: %s\r\nclient port:%d\r\n", mes, ((struct sockaddr_in*)pcliaddr)->sin_port);
+        Sendto(sockfd, mes, n, 0, pcliaddr, len);
+    }
+}
+
+int main(int argc, char**argv){
+    int sockfd;
+    struct sockaddr_in servaddr, cliaddr;
+    sockfd=Socket(AF_INET, SOCK_DGRAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    servaddr.sin_port=htons(SERV_PORT);
+    bind(sockfd, (SA*)&servaddr, sizeof(servaddr));
+
+    dg_echo(sockfd, (SA*)&cliaddr, sizeof(cliaddr));
+}
+
